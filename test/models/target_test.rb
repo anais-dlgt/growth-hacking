2e7bfbea5243fb764cc90ef5dabe5dff4e7cad3b
StarrK <tony.kino.3@gmail.com>
2018-11-14 15:48:46 +0100

    Fix column name in target model

--- test/models/target_test.rb
+++ test/models/target_test.rb
@@ -3,11 +3,19 @@ require 'test_helper'
 class TargetTest < ActiveSupport::TestCase
 
   def setup
-    @target = { email: "foobar@example.com",
-                first_name: "foo",
-                last_name: "bar",
-                type: "student"}
+    @target = Target.new(email: "foobar@example.com",
+                         first_name: "foo",
+                         last_name: "bar",
+                         status: "student")
   end
+
+  test "sould be valid" do
+    assert @target.valid?
+  end
+
+  # email
+
+
 
 
 end
