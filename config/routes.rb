b08b8546257bd37b3d8156b9f6d2b79e0be112cc
Anaïs <anais.diologeant@hotmail.fr>
2018-11-15 10:23:27 +0100

    modif routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root 'home#student'
+  root to: 'home#student'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
